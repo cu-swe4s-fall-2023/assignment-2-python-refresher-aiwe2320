2843a8522b31d3dd53ebeb8e28c4f87cb1ef31e6
Aidan Wegner <aidan.wegner@colorado.edu>
2023-09-14 20:01:01 +0000

    Implement print_fires.py

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,14 @@
+import my_utils
 country='United States of America'
-county_column = 1
-fires_column = 4
+fire_col_name = 'Forest fires'
+country_col_name = 'Area'
+year_col_name = 'Year'
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
-print(fires)
+# get_column(file_name, query_column, query_value, result_column)
+forest_fires = my_utils.get_column(file_name,country_col_name,country,fire_col_name)
+year_list = my_utils.get_column(file_name,country_col_name,country,year_col_name)
+fire_by_year = list(zip(year_list,forest_fires))
+print(f'Total Forest Fires in the {country} by Year:')
+print("Year: Fires")
+for item in fire_by_year:
+    print(f'{item[0]}: {item[1]}')
